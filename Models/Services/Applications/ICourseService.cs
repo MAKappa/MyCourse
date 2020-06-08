d3f6eb9cf335dc221b32e2cfd8cfb8ca49283de6
Arslan Khohar <khurshid.arslan@gmail.com>
2020-06-08 16:15:36 +0200

    last upadate to branch 10

--- Models/Services/Applications/ICourseService.cs
+++ Models/Services/Applications/ICourseService.cs
@@ -1,11 +1,12 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using MyCourse.Models.ViewModels;
 
 namespace MyCourse.Models.Services.Applications
 {
     public interface ICourseService
     {
-         List<CourseViewModel> GetCourses();
+        List<CourseViewModel> GetCourses();
          CourseDetailViewModel GetCourse(int id);
          
     }
